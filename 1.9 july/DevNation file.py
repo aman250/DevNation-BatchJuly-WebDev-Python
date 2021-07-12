e3ae5eecb6130bd85826e037863f42256732838b
unknown <dilawaizkhateja@gmail.com>
2021-07-12 08:45:24 -0700

    I complete the assignment

---
+++ 1.9 july/DevNation file.py
@@ -0,0 +1,63 @@
+print("Question 1")
+print(" ________________")
+print("|               |")
+print("|               |")
+print("|               |")
+print("|               |")
+print("|               |")
+print(" ________________")
+print(" Question 2")
+print("A  write a program that prints the following ")
+num1=1
+num2=2
+num3=3
+num4=4
+num5=5
+num6=6
+num7=7
+num8=8
+num9=9
+num10=10
+num11=11
+num12=12
+num13=13
+num14=14
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+print(num5)
+print(num6)
+print(num7)
+print(num8)
+print(num9)
+print(num10)
+print(num11)
+print(num12)
+print(num13)
+print(num14)
+print("\n\n\nQUESTION 2")
+for i in range(1,15):
+    print(i)
+
+print("\n\n\nQUESTION 3")
+print("\nQUESTION 3 part 1. \n")
+ipt1=input("Enter Your Name ")
+ipt2=input("Enter Your father name ")
+print("Hi Your Name is "+ipt1+" and Your Baba Name is "+ipt2)
+print("\n\nQUESTION 3 part 2. \n")
+ipt1=input("Enter 1st number ")
+ipt2=input("Enter 2nd number ")
+rlt=int(ipt1)+int(ipt2)
+print("Addition of two integer is "+str(rlt))
+
+# side = int(input("Please Enter any Side of a Square  : "))
+#
+# print("Square Star Pattern")
+#
+# for i in range(side):
+#     for i in range(side):
+#         print('*', end = '  ')
+#     print()
+
+
