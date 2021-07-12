02d3a239f4b35c7b5b9e07eff7ba67341013b152
Umair <49642741+umairzafar14@users.noreply.github.com>
2021-07-12 20:24:04 +0500

    First Assignment

---
+++ Assignment - 01/Assignment.py
@@ -0,0 +1,10 @@
+# Question no. 1
+print('----------------')
+print('|              |')
+print('|              |')
+print('|              |')
+print('----------------')
+
+# Question no. 2
+for i in range(1, 15):
+    print(i)
