f01216f679acefb7b44f0fed8db5c673cdb04504
unknown <awanaqib030@gmail.com>
2021-07-12 06:34:55 -0700

    assign no 1

---
+++ assign no 1.py
@@ -0,0 +1,21 @@
+# Ist question
+
+print('__________')
+print('|        |')
+print('__________')
+
+# 2nd questions
+
+print('1')
+print('2')
+print('3')
+print('4')
+print('5...............................')
+print('...14')
+
+# 3rd question
+
+no1 = input('please enter your no')
+no2 = input('please enter your no')
+result = int(no1) + int(no2)
+print('result= ' + str(result))
