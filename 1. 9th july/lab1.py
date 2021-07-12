5ea26304753f088e8415d42b0d22d9f51f04bb0d
zubair <muhammad.zubair.i.am@gmail.com>
2021-07-12 21:02:07 +0500

    lab1 done

---
+++ 1. 9th july/lab1.py
@@ -0,0 +1,29 @@
+#1
+print("--------")
+print("|      |")
+print("|      |")
+print("|      |")
+print("--------")
+
+#2
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+print(11)
+print(12)
+print(13)
+print(14)
+
+#3
+num1=input('enter num1:')
+num2=input('enter num2:')
+result=int(num1)+int(num2)
+
+print("sum="+str(result))
