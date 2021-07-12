46e6380e173e509b8ef3185a24059b79182f352d
unknown <muhammadasimhabib424@gmail.com>
2021-07-12 20:49:48 +0500

    Assignment 1 added

---
+++ 12.july21/q3.py
@@ -0,0 +1,5 @@
+Num1= input("Enter value of first Integer: ")
+Num2= input("Enter value of Second Integer: ")
+# Adding two number
+Sum= float(Num1) + float(Num2)
+print("The sum of {0} and {1} is {2}" .format(Num1,Num2,Sum))
