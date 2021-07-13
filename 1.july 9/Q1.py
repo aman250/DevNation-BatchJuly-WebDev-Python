5dcd3070e86c7364e1c630b8cbb0aa6b32d1438d
Atif <syedatif040@gmail.com>
2021-07-14 00:10:28 +0500

    assignment done

---
+++ 1.july 9/Q1.py
@@ -0,0 +1,8 @@
+#1st question
+print("____________________")
+print("|                  |")
+print("|                  |")
+print("|                  |")
+print("|                  |")
+print("|                  |")
+print("|__________________|")
---
+++ 1.july 9/Q2.py
@@ -0,0 +1,13 @@
+
+#2nd question
+print("1")
+print("2")
+print("3")
+print("4")
+print("5..............................")
+print("...14")
+
+num1 = input("please enter 1st number here: ")
+num2 = input("please enter 2nd number here: ")
+result = int(num1) +int (num2)
+print(result)
