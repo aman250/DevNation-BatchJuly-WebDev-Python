a50da3a041b687ab2c3bf883ac8d744f6454835a
unknown <awanaqib030@gmail.com>
2021-07-14 16:15:28 -0700

    assign no 3

---
+++ assgn no 3/Q1.py
@@ -0,0 +1,9 @@
+
+
+n=int(input('enter no; '))
+fact=1
+while(n>0):
+    fact=fact*n
+    n=n-1
+print('factorial of no is ; ')
+print(fact)
---
+++ assgn no 3/Q2B PART.py
@@ -0,0 +1,20 @@
+
+n = int(4)
+
+i = 1
+while i <= n:
+    j = n
+    while j >= i:
+        print('*', end=' ')
+        j -= 1
+    print()
+    i += 1
+
+
+
+
+
+
+
+
+
