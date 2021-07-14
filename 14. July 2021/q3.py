755348923f1a758240e3c34191cfa69d9a97ca8e
ilyasweb <ilyasweb0721@gmail.com>
2021-07-14 09:55:17 -0700

    Assignment 3 added

---
+++ 14. July 2021/q3.py
@@ -0,0 +1,14 @@
+import math
+total = 0
+more = True
+values = []
+while more:
+    value = input("Enter the input press q to quit")
+    if value == 'q':
+        more = False
+    else:
+        values.append(int(value))
+
+print(sum(values)/len(values))
+
+
