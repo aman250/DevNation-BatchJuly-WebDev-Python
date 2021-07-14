23a71e313d90994ff8432e7e940895328164d672
ilyasweb <ilyasweb0721@gmail.com>
2021-07-14 13:53:27 -0700

    second assignment q2 solution updated

--- 14. July 2021/q2.py
+++ 14. July 2021/q2.py
@@ -1,3 +1,4 @@
+import math
 print('A.')
 for i in range(1, 5):
     for j in range(i):
@@ -6,6 +7,23 @@ for i in range(1, 5):
 print()
 
 print('B.')
+n= int(input("Enter number of rows"))
+rows = math.ceil(row/2)
+for i in range(rows):
+    print(" " * (rows-i) + "*" * (i*2+1))
+for j in range(1, rows):
+    print(" " * (j+1) + (((rows-j)*2-1) * '*'))
+
+print('C.')
+num=4
+for i in range(num,0,-1):
+    for space in range(num-i):
+        print(" ",end="")
+    for n in range(1, 2*i):
+        print(n%2, end="")
+    print()
+'''
+###Part B
 
 #rows = int(input("Enter the odd number of rows you want"))
 rows = 5
@@ -23,8 +41,41 @@ for i in range(n-1, 0, -1):
         print("*", end="")
     print()
 
-print('C.')
+## Part B with while loop
 
+
+a = n+1
+i = 0
+while a>0:
+    j=0
+    k=0
+    while j<n-i:
+        print(" ", end="")
+        j += 1
+    while k < (2*i)-1:
+        print("*", end="")
+        k+=1
+    i+=1
+    a -= 1
+
+    print()
+
+a= n-1
+i = 0
+while a > 0:
+    j = 0
+    k = 0
+    while j < i+1:
+        print(" ", end="")
+        j += 1
+    while k < (2*a) - 1:
+        print("*", end="")
+        k += 1
+    i += 1
+    a -= 1
+
+    print()
+###print('C.')
 num = 4
 for n in range(num):
     for space in range(n):
@@ -34,3 +85,4 @@ for n in range(num):
         if ones != num-n-1:
             print('0',end="")
     print()
+'''
