ba2d38808ef33d2c9f05a0dae4f8505e9e37c9bc
unknown <muhammadasimhabib424@gmail.com>
2021-07-14 16:58:09 +0500

    Assignment 1 added

---
+++ 13.July21/q2.py
@@ -0,0 +1,23 @@
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+print(11)
+print(12)
+print(13)
+print(14)
+
+# using for loop
+Num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+for i in Num1:
+    print(i)
+
+# using for loop through Range function
+for x in range(0, 15):
+    print(x)
---
+++ 13.July21/q3.py
@@ -0,0 +1,11 @@
+# Taking first Int Input from User:
+Num1 = int(input("Enter value of first Integer: "))
+
+# Taking second Int Input from User:
+Num2 = int(input("Enter value of Second Integer: "))
+
+# Adding two number:
+Sum = Num1 + Num2
+
+# Display the sum:
+print(Sum)
