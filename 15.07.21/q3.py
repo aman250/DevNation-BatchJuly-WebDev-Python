6581c59602290e138676f94f38d9b05062f5cc75
wahabwaris <wahabwaris1122@gamil.com>
2021-07-15 21:04:34 +0500

    first assignment

---
+++ 15.07.21/q3.py
@@ -0,0 +1,12 @@
+#first of all we assign the inputs5
+
+
+num1=input("enter the first number: ")
+num2=input("enter the second number: ")
+
+#perform result5
+
+result = int(num1) + int(num2)
+
+#performed results
+print("result:"+ str(result))
