bc25de125d4d1007fd1634ce48681d4e3b9ab6ec
wahabwaris <wahabwaris1122@gamil.com>
2021-07-15 21:55:04 +0500

    second assignment

--- 15.07.21/q2.py
+++ 15.07.21/q2.py
@@ -1,4 +1,8 @@
-#program to print 0 to 14
+a = int(input("number of classes held;"))
+b = int(input("number of classes attended;"))
+percentage = (b/a)*100
 
-for i  in range(1,15):
-    print(i)
+if percentage>=75:
+    print("the students is allowed to sit in the exam hall")
+else:
+     print("the students is not allowed to sit in the exam hall")
