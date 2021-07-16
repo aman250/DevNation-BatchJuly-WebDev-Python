55967ffcad42c945b508a24b1b74f43562067af9
Zaeem <zaeemsaleem@protonmail.ch>
2021-07-16 10:10:01 +0500

    Completed q1 and two parts  of q2

---
+++ 3.July14/q2.py
@@ -0,0 +1,38 @@
+# printing right triangle
+for i in range(1, 5):
+    for j in range(i):
+        print('*', end='')
+    print('')
+
+
+# printing diamond
+stop = 1
+ignore = True
+flag = True
+for i in range(-5, 6, 2):
+    # calculating the limit for internal loop printing spaces
+    internal_stop = int((abs(i)-1)/2)
+    # skipping the printing of extra line
+    if internal_stop == 0:
+        if ignore:
+            ignore = False
+            continue
+    # loop to print spaces
+    for j in range(0, internal_stop):
+        print(" ", end="")
+    # loop to print *
+    for k in range(stop):
+        print("*", end="")
+    print("")
+    # reversing the limiting point for * printing loop
+    if stop < 5:
+        if flag:
+            stop += 2
+        else:
+            stop -= 2
+    else:
+        stop -= 2
+        flag = False
+
+
+# prining reverse pyramid of 1,0
