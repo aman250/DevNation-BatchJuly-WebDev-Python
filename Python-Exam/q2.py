ba1b2e4dfc4f433f37ee3127b4466781dda14b23
Alwaz <alwazkazi3@gmail.com>
2021-07-30 10:35:06 -0700

    question 2 complete

---
+++ Python-Exam/q2.py
@@ -0,0 +1,38 @@
+
+
+
+#### Take tow inputs from user
+first_number = input('Enter first number: ')
+
+second_number = input('Enter second number: ')
+
+### Take operand as input
+operand = input('Enter operand: ')
+
+
+
+### print the result according to operand entered by user
+if operand == '+':
+    result = int(first_number)+int(second_number)
+    print("Addion of " + str(first_number) + ' and '+ str(second_number)+' is ' + str(result))
+
+elif operand == '-':
+    result = int(first_number) - int(second_number)
+    print("Addion of " + str(first_number) + ' and ' + str(second_number) + ' is ' + str(result))
+
+elif operand == '*':
+    result = int(first_number) * int(second_number)
+    print("Addion of " + str(first_number) + ' and ' + str(second_number) + ' is ' + str(result))
+
+elif operand == '/':
+    result = int(first_number) / int(second_number)
+    print("Addion of " + str(first_number) + ' and ' + str(second_number) + ' is ' + str(result))
+
+elif operand == '%':
+    result = int(first_number) % int(second_number)
+    print("Addion of " + str(first_number) + ' and ' + str(second_number) + ' is ' + str(result))
+
+else:
+    print('Please enter a valid operand...!')
+
+
