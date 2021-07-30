4e043b2a5b2d25efe97d66bb455e5673dca7282c
Alwaz <alwazkazi3@gmail.com>
2021-07-30 11:50:50 -0700

    Question 4 complete

---
+++ Python-Exam/q4.py
@@ -0,0 +1,15 @@
+
+
+### Calculating Factorial
+num = int(input('Enter number whose factorial needs to be determined: '))
+
+if num == 0 or num == 1:
+    print(num,'! = ',1)
+
+else:
+    fact = 1
+    for i in range(1 , num+1):
+        print(fact)
+        fact *= i
+    print(num,'! =', fact)
+
