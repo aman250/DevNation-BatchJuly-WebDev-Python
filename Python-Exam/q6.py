6c00d6b5054b2fb576aac5df2cab9b5d6747240a
Alwaz <alwazkazi3@gmail.com>
2021-07-30 10:57:24 -0700

    Question 6 complete

---
+++ Python-Exam/q6.py
@@ -0,0 +1,10 @@
+
+###Create a tuple
+my_tuple=(10,20,30,40,50,60)
+
+# Access item at index 3
+print('Item at index 3: ', my_tuple[3])
+
+# iterating through tuple
+for items in my_tuple:
+    print(items,end=' ')
