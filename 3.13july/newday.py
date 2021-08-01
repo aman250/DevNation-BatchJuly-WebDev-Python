1ec7303bed58d689203b6ba7b6e308eb30b6873d
gullhassan23 <hassanxhan1433@gmail.com>
2021-08-01 22:09:34 +0500

    first python exam

--- 3.13july/newday.py
+++ 3.13july/newday.py
@@ -1,13 +1,13 @@
-a= int(input("Enter a number  "))
+a= input(int("Enter a number  "))
 
 f = 1
 
 if a < 0:
     print("Factorial cant defined for negative integer")
-elif (a == 0):
+elif a == 0:
     print("The factorial of 0 is 1")
 else:
-    while (a > 0):
+    while a > 0:
         f = f * a
 
         a = a - 1
