39da6c4c3c8aeaa3e68bbce9686c7273587b016d
unknown <muhammadasimhabib424@gmail.com>
2021-08-03 12:10:28 +0500

    Python 3rd Assignment

---
+++ 30.July21/Q1.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number: "))
+i = 1
+fac = 1
+while i <= num:
+  fac = fac*i
+  i+=1
+print(fac)
---
+++ 30.July21/Q2b.py
@@ -0,0 +1,35 @@
+num = int(input("Enter a Number: "))
+#upper triangle
+a=1
+d=1
+#lopp will run "num" times
+while a<=num:
+  b=1
+  #will add empty spaces one less into "num" times
+  while b<=num-a:
+    print(" ",end="")
+    b+=1
+  c=1  
+  while c<=d:
+    print("*", end="")
+    c+=1
+  print(" ")
+  d+=2
+  a+=1
+
+  #lower triangle
+i=1
+k=1
+while i<=num-1:
+  j=1  
+  while j<=k:
+    print(" ", end="")
+    j+=1
+  k+=1 
+  l=1
+  while l<=(2*(num-i)-1):
+    print('*', end = '')
+    l+=1 
+  print(" ")
+  i+=1
+    
