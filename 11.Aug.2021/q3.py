8e758f65c35081c0d2c4dfcfd5df66469b637518
Alwaz <alwazkazi3@gmail.com>
2021-08-11 09:49:13 -0700

    assignment complete

--- 11.Aug.2021/q3.py
+++ 11.Aug.2021/q3.py
@@ -1,2 +1,18 @@
 
+# function to check whether average is even or odd
+def avg_even_or_odd(avg):
+    print('Average of given list is: ',round(avg))
+    if (avg % 2 == 0):
+        print(avg,' is an even number')
+    else:
+        print(avg, 'is an odd number')
 
+# function to calculate average of list
+def calc_avg(nums):
+    avg=sum(nums)/len(nums)
+    return avg_even_or_odd(round(avg))
+
+
+
+### Function call
+calc_avg([7,3,3,6])
