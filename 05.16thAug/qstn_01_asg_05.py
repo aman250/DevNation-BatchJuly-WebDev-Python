b04db72952e4fd68554c79b2a84b79521d874d4c
Marwa-Khan <marwakhan211@gmail.com>
2021-08-17 01:14:32 +0500

    Lab Assignment_05

---
+++ 05.16thAug/qstn_01_asg_05.py
@@ -0,0 +1,24 @@
+# Question_01
+print("\n\n\t\t\t----------------Program to find the power of given number------------------\n\n")
+
+# function definition
+def power(base_num, power_num):
+    result = 1
+    #power_num as passed as an argument to iterate the loop for that number of times
+    for i in range(power_num):
+        result = result*base_num
+    print("The power of "+str(base_num)+" is: "+str(result))
+
+
+
+#user defined Inputs
+num1=int(input("Enter the base to find power of that number:"))
+num2=int(input("Enter the power value:"))
+
+#function calling
+power(num1, num2)
+
+
+
+
+
---
+++ 05.16thAug/qstn_02_asg_05.py
@@ -0,0 +1,32 @@
+#Question_02
+print("\n\n\t\t\t----------------Program to replace the vowel letters from sentence with the letter "
+      "the g------------------\n\n")
+
+#function definition
+def replace_vowels(string):
+    count = 0
+    for letter in string:
+        if letter in vowels_list:
+            count += 1
+            string = string.replace(letter, "g")
+
+    if count > 0:
+        print("The sentence after replacing vowels with the letter g is: " + string)
+    else:
+        print("The sentence has no vowels ")
+
+
+#input from user
+string=input("Enter the sentence:")
+# vowels list
+vowels_list=['A','E','I','O','U','a','e','i','o','u']
+#function calling
+replace_vowels(string)
+
+
+
+
+
+
+
+
